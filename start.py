1bbd049a08603e7d93985864a8b1726df730faff
dharmhello <155912524+dharmhello@users.noreply.github.com>
2024-08-02 17:19:35 +0530

    Update start.py

--- start.py
+++ start.py
@@ -6,4 +6,4 @@ print("Hi, I am shyama13")
 print("Hi, I am Rama in ayodhya.")
 
 print("Hi, I am Mohit ddss66dhhfddwdd")
-print("Hi, I am Mohit ddss66dhhfddwd   duuu469")
+print("Hi, I am Mohit ddss66dhhfddwd   duuu4698")
